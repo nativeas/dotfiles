6b791d829288e539faf6441d6f011aa83b63e742
nativeas <saicn@qq.com>
2013-09-04 21:47:31 +0800

    a

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+ln -s  ~/dotfiles/gitconfig ~/.gitconfig
+ln -s  ~/dotfiles/gitignore_global ~/.gitignore_global
+ln -s  ~/dotfiles/vim ~/.vim
+ln -s  ~/dotfiles/vim/vimrc ~/.vimrc
---
+++ vim/.vim
@@ -0,0 +1,1 @@
+/Users/nativeas/.vim
